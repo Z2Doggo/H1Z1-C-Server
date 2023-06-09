5b10e13fe9437b72b5229e78cd20b67344fba4d8
doggo <xcykrim0@gmail.com>
2023-06-09 15:58:19 -0500

    .

--- src/shared/session.h
+++ src/shared/session.h
@@ -147,20 +147,5 @@ struct Session_State
 
 	u64 creation_date;
 	u64 last_login_date;
-
-	i32 stance;
-	i32 position[3];
-	i32 unknown12_f32[3];
-	i32 rotation[4];
-	i32 rotationRaw[4];
-	i32 lookAt[4];
-	f32 orientation;
-	i32 direction;
-	i32 front_tilt;
-	i32 side_tilt;
-	i32 angle_change;
-	i32 vertical_speed;
-	i32 horizontal_speed;
-	i32 engine_rpm;
 	// end world_character struct
 };
