969c12745934eb3f66ef3f8856bbef60d326cc3a
doggo <lane.waguespack@gmail.com>
2023-08-17 02:00:59 -0500

    .

--- src/data/loginData/loginCharacterData.c
+++ src/data/loginData/loginCharacterData.c
@@ -1,1 +1,1 @@
-// shared functions for login packets as well as storing data in session state variables
+// shared functions for login packets as well as storing data in session state variables for use in other packets besides login packets, i.e. characterName, characterId, etc
